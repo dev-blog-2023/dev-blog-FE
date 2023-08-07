92cdbf9aba65b76a1c0f78a982998ac8f8d6c642
leemiran0729 <leemiran.0729@gmail.com>
2023-08-07 17:39:16 +0900

    fix: Main 컴포넌트 import 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { Main } from "./components/Main";
+import Main from "./components/Main";
 
 function App() {
   return (
