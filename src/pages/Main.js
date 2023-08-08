bcdea30fcf10d171df417b86dabbec532d869161
leemiran0729 <leemiran.0729@gmail.com>
2023-08-08 14:04:50 +0900

    feat: 필요한 스타일 컴포넌트 생성

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,34 @@
+import styled from "styled-components";
 import React from "react";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Nav />
+      <SignInBtn>Login</SignInBtn>
+      <Card />
+    </div>
+  );
 };
 
 export default Main;
+
+const Nav = styled.div`
+  height: 50px;
+  background-color: #000;
+`;
+
+const SignInBtn = styled.button`
+  width: 122px;
+  height: 40px;
+  color: #000;
+  background-color: #fff;
+  border-radius: 10px;
+`;
+
+const Card = styled.div`
+  width: 243px;
+  height: 283px;
+  border: 1px solid #000;
+  border-radius: 10px;
+`;
