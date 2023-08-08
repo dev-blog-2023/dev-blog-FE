63f481265c769280dcadeb0a86b868be1d40e2d3
leemiran0729 <leemiran.0729@gmail.com>
2023-08-08 22:41:04 +0900

    style: 기존 Login 컴포넌트 Modal 스타일 제거

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -36,42 +36,6 @@ const Login = ({ isShowing, hide }) =>
 
 export default Login;
 
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1040;
-  width: 100vw;
-  height: 100vh;
-  background-color: #000;
-  opacity: 0.5;
-`;
-
-const ModalWrapper = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1050;
-  width: 100%;
-  height: 100%;
-  overflow-x: hidden;
-  overflow-y: auto;
-  outline: 0;
-`;
-
-const PopupContainer = styled.div`
-  z-index: 100;
-  background: white;
-  position: relative;
-  margin: 1.75rem auto;
-  width: 648px;
-  height: 414px;
-  border-radius: 10px;
-  background-color: #fff;
-  border: 1px solid #000;
-  display: flex;
-`;
-
 const LogoContainer = styled.div`
   border-top-left-radius: 10px;
   background-color: #000;
