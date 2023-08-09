6149ef3f4e4120deb3508f66348e172365136d14
leemiran0729 <leemiran.0729@gmail.com>
2023-08-09 20:46:34 +0900

    feat: MyPage 내 정보 수정 UI 구현

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Nav from "../components/Nav";
 import Title from "../components/Title";
+import Modal from "../components/Modal";
+import useModal from "../hooks/useModal";
+import Text from "../components/Text";
 
 const MyPage = () => {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const { isShowing, toggle } = useModal();
+
+  const handleUpdate = () => {
+    setIsDisabled(() => false);
+  };
+
   return (
     <MyPageContainer>
       <Nav />
       <MyInfoContainer>
         <Title>My Info</Title>
         <ButtonContainer>
-          <Button color="#000">내 정보 수정</Button>
+          <Button color="#000" onClick={handleUpdate}>
+            내 정보 수정
+          </Button>
           <Button color="#FF0000">탈퇴하기</Button>
         </ButtonContainer>
         <InputContainer>
           <Label>username</Label>
-          <Input />
+          <Input disabled={isDisabled} />
         </InputContainer>
         <InputContainer>
           <Label>name</Label>
-          <Input />
+          <Input disabled={isDisabled} />
         </InputContainer>
         <InputContainer>
           <Label>email</Label>
-          <Input />
+          <Input disabled={isDisabled} />
         </InputContainer>
+        {!isDisabled ? (
+          <CompleteBtn onClick={toggle}>수정완료</CompleteBtn>
+        ) : null}
       </MyInfoContainer>
+      {isShowing ? (
+        <Modal isShowing={isShowing} hide={toggle} width="321px" height="161px">
+          <ModalBtnContainer>
+            <CancelBtn onClick={toggle}>X</CancelBtn>
+          </ModalBtnContainer>
+          <CompleteContainer>
+            <LogoImg
+              src="https://cdn1.iconfinder.com/data/icons/social-black-buttons/512/blog-512.png"
+              alt="logo"
+            />
+            <Text fontSize="12px">내 정보가 수정이 완료되었습니다.</Text>
+          </CompleteContainer>
+        </Modal>
+      ) : null}
     </MyPageContainer>
   );
 };
@@ -62,6 +91,8 @@ const Button = styled.button`
   margin-right: 10px;
   &:hover {
     cursor: pointer;
+    color: #fff;
+    background-color: ${(props) => props.color};
   }
 `;
 
@@ -81,4 +112,48 @@ const Input = styled.input`
   height: 35px;
   border-radius: 5px;
   border: 1px solid #848484;
+`;
+
+const CompleteContainer = styled.div`
+  margin-left: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LogoImg = styled.img`
+  width: 62px;
+  height: 62px;
+`;
+
+const ModalBtnContainer = styled.div`
+  display: inline-block;
+  position: absolute;
+  right: 10px;
+  top: 0px;
+`;
+
+const CancelBtn = styled.button`
+  margin-top: 10px;
+  outline: none;
+  border: none;
+  background-color: #fff;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const CompleteBtn = styled.button`
+  width: 305px;
+  height: 36px;
+  color: #fff;
+  background-color: #000;
+  border: none;
+  border-radius: 5px;
+  margin: 30px auto;
+  &:hover {
+    cursor: pointer;
+  }
 `;
