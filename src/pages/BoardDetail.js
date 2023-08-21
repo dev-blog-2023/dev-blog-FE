5f3e71fcc133daf16f4faeb464a33bd66499e2d7
leemiran0729 <leemiran.0729@gmail.com>
2023-08-21 16:39:37 +0900

    feat: 해당 유저일 때만 게시글 수정 삭제 가능하게 구현

--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Title from "../components/Title";
 import Text from "../components/Text";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const BoardDetail = () => {
   const { boardId } = useParams();
+  const [title, setTitle] = useState("");
+  const [writer, setWriter] = useState("");
+  const [content, setContent] = useState("");
+  const [writeDate, setWriteDate] = useState("");
+  const [comment, setComment] = useState([]);
+  const navigate = useNavigate();
+  const loginData = JSON.parse(window.sessionStorage.getItem("loginUser"));
+
+  useEffect(() => {
+    axios({
+      url: `http://52.79.222.161:8080/board/${boardId}`,
+      method: "get",
+      headers: {},
+    })
+      .then(function (response) {
+        setTitle(response.data.title);
+        setContent(response.data.content);
+        setWriter(response.data.writer);
+        setWriteDate(response.data.createDateTime.substr(0, 10));
+        setComment(response.data.commentList);
+      })
+      .catch((error) => {
+        alert("게시글을 불러올 수 없습니다.");
+      });
+  }, []);
 
   return (
     <BoardContainer>
@@ -16,27 +42,43 @@ const BoardDetail = () => {
         />
       </LogoContainer>
       <DetailContainer>
-        <Title textAlign="left">첫번째 블로그 제목입니다.</Title>
-        <ListBtn>목록으로</ListBtn>
+        <Title textAlign="left">{title}</Title>
+        <ButtonContainer>
+          {loginData.username === writer && (
+            <>
+              <ListBtn>수정</ListBtn>
+              <ListBtn>삭제</ListBtn>
+            </>
+          )}
+
+          <ListBtn onClick={() => navigate("/")}>목록으로</ListBtn>
+        </ButtonContainer>
         <Img />
         <InfoContainer>
-          <Text marginTop="10px">작성자: 이미란</Text>
-          <Text marginTop="2px">작성일자: 2023-08-08</Text>
+          <Text marginTop="10px">작성자: {writer}</Text>
+          <Text marginTop="2px">작성일자: {writeDate}</Text>
         </InfoContainer>
-        <ContentContainer>
-          내용입니다.
-          <br />
-          내용입니다.
-        </ContentContainer>
+        <ContentContainer>{content}</ContentContainer>
         <CommentImg
           src="https://cdn0.iconfinder.com/data/icons/evericons-24px-vol-1/24/comment-text-256.png"
           alt="commentImg"
         />
-        <CommentInfoContainer>
-          <Text marginTop="10px">작성자: 기범</Text>
-          <Text marginTop="10px">작성일자: 2023-08-09</Text>
-        </CommentInfoContainer>
-        <Comment>Comment.....</Comment>
+        {comment &&
+          comment.map((item) => (
+            <div key={item.id}>
+              <CommentInfoContainer>
+                <Text marginTop="10px">작성자: {item.writer}</Text>
+                <Text marginTop="10px">
+                  작성일자: {item.createDateTime.substr(0, 10)}
+                </Text>
+              </CommentInfoContainer>
+              <Comment>{item.content}</Comment>
+              {item.childList &&
+                item.childList.map((child) => (
+                  <ChildComment key={child.id}>{child.content}</ChildComment>
+                ))}
+            </div>
+          ))}
       </DetailContainer>
     </BoardContainer>
   );
@@ -79,7 +121,6 @@ const ListBtn = styled.button`
   background-color: #fff;
   border-radius: 10px;
   text-align: center;
-  margin-right: 600px;
   margin-top: 5px;
   border: 1px solid #000;
   &:hover {
@@ -125,4 +166,16 @@ const Comment = styled.div`
   border-radius: 5px;
   border: 1px solid #848484;
   width: 635px;
+`;
+
+const ChildComment = styled.div`
+  border-radius: 5px;
+  border: 1px solid #848484;
+  width: 635px;
+  margin-left: 20px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 10px;
 `;
